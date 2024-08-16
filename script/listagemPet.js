ded7d5df0d1cae273c9ea66689ac59fbc5660a11
Lucas Benkendorf <benklucass@gmail.com>
2024-08-16 20:08:53 -0300

    criado hiperlink para mudar de pagina

--- script/listagemPet.js
+++ script/listagemPet.js
@@ -5,19 +5,27 @@ function carregarDados() {
 
     animaisNaMemoria.forEach((animal) => {
 
+
+
         document.createElement("h1")
         lista.appendChild(meuH1)
+
+
 
         const div = document.createElement("div")
         div.classList.add("item-pet")
         const img = document.createElement('img')
         img.setAttribute("width", "200px")
-        img.setAttribute("src", "https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&w=600")
+        img.style.objectFit = "cover"
+        img.setAttribute("src", "animal.foto")
         div.append(img)
 
-        const h2 = document.createElement("h2")
 
-        h2.innerText = "Gato Dirceu"
+
+        const h2 = document.createElement("h2")
+        h2.innerText = animal.nome
+
+
 
         const button = document.createElement("button")
         button.innetText = "Adicionar"
@@ -26,11 +34,6 @@ function carregarDados() {
         lista.appendChild(div)
 
     })
-
-
-
 }
-
-
 
 document.addEventListener('DOMContentLoaded', carregarDados) // quando a tela for renderizada vai disparar a funcao
