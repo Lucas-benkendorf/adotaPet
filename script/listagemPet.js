fabf6c640548b7564060ac79d8f7e9ca0919cc6d
Lucas Benkendorf <benklucass@gmail.com>
2024-08-16 21:40:29 -0300

    adicionado o botão para ser excluido os animais

--- script/listagemPet.js
+++ script/listagemPet.js
@@ -1,3 +1,17 @@
+function deletar(id) {
+// 1- ir no local e busca o array de animais
+const animaisAtuaisNaMemoria = JSON.parse(localStorage.getItem("animais"))
+const animaisFiltrados = animaisAtuaisNaMemoria.filter((item) => item.id !== id)
+
+localStorage.setItem("animais", JSON.stringify(animaisFiltrados))
+
+document.getElementById("lista-animais").innerText = ""
+carregarDados()
+}
+
+
+
+
 function carregarDados() {
     const animaisNaMemoria = JSON.parse(localStorage.getItem('animais'))
     const lista = document.getElementById("lista-animais")
@@ -28,7 +42,8 @@ function carregarDados() {
 
 
         const button = document.createElement("button")
-        button.innetText = "Adicionar"
+        button.innetText = "Deletar"
+        button.onclick = () => deletar(pet.id)
         div.append(button)
 
         lista.appendChild(div)
