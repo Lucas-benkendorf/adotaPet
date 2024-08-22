4e9344fe34978ab42260c3df148739a772a8db85
Lucas Benkendorf <benklucass@gmail.com>
2024-08-21 22:03:40 -0300

    criado funcao de find para a tela de detalhes do pet

---
+++ script/detalhes.js
@@ -0,0 +1,19 @@
+function carregarDetalhesAnimais(){
+const animaisLista =JSON.parse(localStorage.getItem("animais"))
+
+const url = new URL (window.location.href)
+const params = new URLSearchParams(url.search)
+const idURL = params.get('id')
+
+
+const animalEncontrado = animaisLista.find((item) => item.id == id )
+document.getElementById("nome").innerText = animalEncontrado.nome
+document.getElementById("foto").setAttribute("src", animalEncontrado.foto)
+document.getElementById("idade").innerText = animalEncontrado.idade
+document.getElementById("cor").innerText = animalEncontrado.cor
+document.getElementById("especie").innerText = animalEncontrado.especie
+document.getElementById("descricao").innerText = animalEncontrado.descricao
+
+}
+
+document.addEventListener("DOMContentLoaded", carregarDetalhesAnimais)
