26650ad51100587ed02120b19a66ade7661cc679
Lucas Benkendorf <benklucass@gmail.com>
2024-08-22 20:20:01 -0300

    adicionado detalhes

--- script/detalhes.js
+++ script/detalhes.js
@@ -7,6 +7,12 @@ const idURL = params.get('id')
 
 
 const animalEncontrado = animaisLista.find((item) => item.id == id )
+if(animalEncontrado === undefined){
+    document.getElementById('mensagemError').style.display = "block"
+    document.getElementById("conteudo").style.display = 'none !important'
+
+}else{
+   
 document.getElementById("nome").innerText = animalEncontrado.nome
 document.getElementById("foto").setAttribute("src", animalEncontrado.foto)
 document.getElementById("idade").innerText = animalEncontrado.idade
@@ -14,6 +20,8 @@ document.getElementById("cor").innerText = animalEncontrado.cor
 document.getElementById("especie").innerText = animalEncontrado.especie
 document.getElementById("descricao").innerText = animalEncontrado.descricao
 
-}
+}}
+
+
 
 document.addEventListener("DOMContentLoaded", carregarDetalhesAnimais)
